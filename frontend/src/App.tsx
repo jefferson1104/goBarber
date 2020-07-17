ac8947ccbb6a27f8314df7487759cc8e016dc6b3
jefferson <jefferson1104junior@gmail.com>
2020-07-17 19:17:47 -0300

    LogIn and LogOut

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,13 +3,13 @@ import GlobalStyle from './styles/global';
 // import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
 
-import AuthContext from './context/AuthContext';
+import { AuthProvider } from './hooks/AuthContext';
 
 const App: React.FC = () => (
   <>
-    <AuthContext.Provider value={{ name: 'Jefferson' }}>
+    <AuthProvider>
       <SignIn />
-    </AuthContext.Provider>
+    </AuthProvider>
 
     <GlobalStyle />
   </>
---
+++ frontend/src/hooks/AuthContext.tsx
@@ -0,0 +1,73 @@
+import React, { createContext, useCallback, useState, useContext } from 'react';
+import api from '../services/api';
+
+interface AuthState {
+  token: string;
+  user: object;
+}
+
+interface SignInCredentials {
+  email: string;
+  password: string;
+}
+
+interface AuthContextData {
+  user: object;
+  signIn(credentials: SignInCredentials): Promise<void>;
+  signOut(): void;
+}
+
+const AuthContext = createContext<AuthContextData>({} as AuthContextData);
+
+const AuthProvider: React.FC = ({ children }) => {
+  // estado onde contem dados do login ou dados do localstorage caso houver
+  const [data, setData] = useState<AuthState>(() => {
+    const token = localStorage.getItem('@goBarber:token');
+    const user = localStorage.getItem('@goBarber:user');
+
+    if (token && user) {
+      return { token, user: JSON.parse(user) };
+    }
+
+    return {} as AuthState;
+  });
+
+  // fazendo login na aplicação
+  const signIn = useCallback(async ({ email, password }) => {
+    const response = await api.post('sessions', { email, password });
+
+    const { token, user } = response.data;
+
+    localStorage.setItem('@goBarber:token', token);
+    localStorage.setItem('@goBarber:user', JSON.stringify(user));
+
+    // salvando dados de login no estado
+    setData({ token, user });
+  }, []);
+
+  // fazendo logout na aplicação
+  const signOut = useCallback(() => {
+    localStorage.removeItem('@goBarber:token');
+    localStorage.removeItem('@goBarber:user');
+
+    setData({} as AuthState);
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+function useAuth(): AuthContextData {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+
+  return context;
+}
+
+export { AuthProvider, useAuth };
